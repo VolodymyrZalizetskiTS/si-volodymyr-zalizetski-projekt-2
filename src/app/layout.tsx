bb4701674231717903e8ebbc392e87b3e5a087c4
SirOxygenFish <vovazalizetski@gmail.com>
2025-12-17 09:36:56 +0100

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://necolas.github.io/normalize.css/7.0.0/normalize.css"
+        />
+      </head>
       <body
         className={`${montserrat.variable} antialiased bg-black text-white`}
       >
